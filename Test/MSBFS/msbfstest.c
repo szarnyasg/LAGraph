bf4e85a4394ea66f6543f239f8d8aa56efc64354
Gabor Szarnyas <szarnyasg@gmail.com>
2020-06-05 23:56:22 +0200

    component size

--- Test/MSBFS/msbfstest.c
+++ Test/MSBFS/msbfstest.c
@@ -78,15 +78,17 @@ void print_bit_matrix(const GrB_Matrix A) {
     printf("\n");
 }
 
-void print_bit_matrices(const GrB_Matrix frontier, const GrB_Matrix next, const GrB_Matrix seen, const GrB_Vector popcount, const GrB_Vector sp) {
+void print_bit_matrices(const GrB_Matrix frontier, const GrB_Matrix next, const GrB_Matrix seen, const GrB_Vector next_popcount, const GrB_Vector sp) {
     GrB_Index nrows, ncols;
     GrB_Matrix_nrows(&nrows, frontier);
     GrB_Matrix_ncols(&ncols, frontier);
-    printf("          frontier             next               seen         popcount    sp\n");
+    printf("          frontier             next               seen         next_popcount    sp\n");
     for (GrB_Index i = 0; i < nrows; i++) {
         printf("%4ld:", i);
         for (GrB_Index j = 0; j < ncols; j++) {
-            printf(" %016lx   %016lx   %016lx   %8ld   %3ld", extract(frontier, i, j), extract(next, i, j), extract(seen, i, j), extract_v(popcount, i), extract_v(sp, i));
+            printf(
+              " %016lx   %016lx   %016lx   %8ld   %3ld",
+              extract(frontier, i, j), extract(next, i, j), extract(seen, i, j), extract_v(next_popcount, i), extract_v(sp, i));
         }
         printf("\n");
     }
@@ -117,28 +119,7 @@ GrB_Info create_diagonal_bit_matrix(GrB_Matrix D) {
     return info;
 }
 
-GrB_Info create_ones_vector(GrB_Vector v) {
-    GrB_Info info;
-
-    GrB_Index n;
-    GrB_Vector_size(&n, v);
-
-//    I = 0, 1, ..., n
-//    X = 1, 1, ..., 1
-    GrB_Index* I = LAGraph_malloc(n, sizeof(GrB_Index));
-    uint64_t*  X = LAGraph_malloc(n, sizeof(uint64_t));
-
-    for (GrB_Index k = 0; k < n; k++) {
-        I[k] = k;
-        X[k] = 1L;
-    }
-    GrB_Vector_build(v, I, X, n, GrB_PLUS_UINT64);
-
-    return info;
-}
-
-
-uint64_t popcount(uint64_t x)
+uint64_t next_popcount(uint64_t x)
 {
     int c = 0;
     for (; x != 0; x >>= 1)
@@ -149,7 +130,7 @@ uint64_t popcount(uint64_t x)
 
 void fun_sum_popcount (void *z, const void *x)
 {
-    (*((uint64_t *) z))  = popcount(* ((uint64_t *) x));
+    (*((uint64_t *) z))  = next_popcount(* ((uint64_t *) x));
 }
 
 
@@ -160,22 +141,22 @@ int main (int argc, char **argv)
     //--------------------------------------------------------------------------
 
     GrB_Info info ;
-    GrB_Matrix A = NULL, frontier = NULL, next = NULL, seen = NULL, not_seen = NULL ;
+    GrB_Matrix A = NULL, frontier = NULL, next = NULL, seen = NULL, not_seen = NULL, Seen_PopCount = NULL ;
 
-    GrB_Matrix PopCount;
-    GrB_Vector popcount;
-    uint64_t total_popcount;
+    GrB_Matrix Next_PopCount;
+    GrB_Vector next_popcount;
+    uint64_t total_next_popcount;
 
-    GrB_Vector ones, level_v, sp;
+    GrB_Vector ones, level_v, sp, compsize;
 
     LAGraph_init ( ) ;
 
-    // initializing unary operator for popcount
+    // initializing unary operator for next_popcount
     GrB_UnaryOp op_popcount = NULL ;
     LAGRAPH_OK (GrB_UnaryOp_new(&op_popcount, fun_sum_popcount, GrB_UINT64, GrB_UINT64))
 
     // create the input matrix
-    const GrB_Index n = 6;
+    const GrB_Index n = 8;
     const GrB_Index bit_matrix_ncols = (n+63)/64;
 
     LAGr_Matrix_new(&A, GrB_UINT64, n, n)
@@ -188,24 +169,22 @@ int main (int argc, char **argv)
     LAGr_Matrix_setElement(A, val, 1, 3)
     LAGr_Matrix_setElement(A, val, 2, 4)
     LAGr_Matrix_setElement(A, val, 3, 5)
+    LAGr_Matrix_setElement(A, val, 6, 7)
     // lower triangle
-    LAGr_Matrix_setElement(A, val, 2, 0)
-    LAGr_Matrix_setElement(A, val, 3, 0)
-    LAGr_Matrix_setElement(A, val, 2, 1)
-    LAGr_Matrix_setElement(A, val, 3, 1)
-    LAGr_Matrix_setElement(A, val, 4, 2)
-    LAGr_Matrix_setElement(A, val, 5, 3)
+    GrB_eWiseAdd(A, NULL, NULL, GrB_PLUS_UINT64, A, A, LAGraph_desc_otoo);
 
     LAGr_Matrix_new(&frontier, GrB_UINT64, n, bit_matrix_ncols)
     LAGr_Matrix_new(&next, GrB_UINT64, n, bit_matrix_ncols)
     LAGr_Matrix_new(&seen, GrB_UINT64, n, bit_matrix_ncols)
     LAGr_Matrix_new(&not_seen, GrB_UINT64, n, bit_matrix_ncols)
-    LAGr_Matrix_new(&PopCount, GrB_UINT64, n, bit_matrix_ncols)
+    LAGr_Matrix_new(&Next_PopCount, GrB_UINT64, n, bit_matrix_ncols)
+    LAGr_Matrix_new(&Seen_PopCount, GrB_UINT64, n, bit_matrix_ncols)
 
-    LAGr_Vector_new(&popcount, GrB_UINT64, n)
+    LAGr_Vector_new(&next_popcount, GrB_UINT64, n)
     LAGr_Vector_new(&ones, GrB_UINT64, n)
     LAGr_Vector_new(&level_v, GrB_UINT64, n)
     LAGr_Vector_new(&sp, GrB_UINT64, n)
+    LAGr_Vector_new(&compsize, GrB_UINT64, n)
 
     // initialize frontier matrix
 //    LAGr_Matrix_setElement(frontier, 1L << 63, 0, 0)
@@ -213,10 +192,10 @@ int main (int argc, char **argv)
     // to compute closeness centrality, start off with the diagonal as a frontier
     create_diagonal_bit_matrix(frontier);
 
-    // initialize ones vector
-    create_ones_vector(ones);
+    // initialize vectors with 1s
+    GrB_assign(ones, NULL, NULL, 1, GrB_ALL, n, NULL);
 
-        // initialize seen matrix with all explicit zeros...
+    // initialize seen matrix with all explicit zeros...
     for (GrB_Index i = 0; i < n; i++) {
         for (GrB_Index j = 0; j < bit_matrix_ncols; j++) {
             LAGr_Matrix_setElement(seen, 0, i, j)
@@ -238,27 +217,30 @@ int main (int argc, char **argv)
         // next = next & ~seen // n.b. masking is not applicable
         LAGr_eWiseMult(next, NULL, NULL, GrB_BAND_UINT64, next, not_seen, NULL)
 
-        GrB_Matrix_apply(PopCount, NULL, NULL, op_popcount, next, NULL);
-        LAGr_reduce(popcount, NULL, NULL, GxB_PLUS_UINT64_MONOID, PopCount, NULL)
-        LAGr_reduce(&total_popcount, NULL, GxB_PLUS_UINT64_MONOID, popcount, NULL)
-        if (total_popcount == 0) {
+        GrB_Matrix_apply(Next_PopCount, NULL, NULL, op_popcount, next, NULL);
+        LAGr_reduce(next_popcount, NULL, NULL, GxB_PLUS_UINT64_MONOID, Next_PopCount, NULL)
+        LAGr_reduce(&total_next_popcount, NULL, GxB_PLUS_UINT64_MONOID, next_popcount, NULL)
+        if (total_next_popcount == 0) {
             printf("no new vertices found\n");
             break;
         }
-
-//        GrB_Matrix_apply(popcount, NULL, NULL, GxB_PLUS_UINT64_MONOID, PopCount, NULL);
-        LAGr_eWiseMult(popcount, NULL, NULL, GrB_TIMES_UINT64, popcount, level_v, NULL)
-        LAGr_eWiseAdd(sp, NULL, NULL, GrB_PLUS_UINT64, sp, popcount, NULL)
-
         // seen = seen | next
         LAGr_eWiseAdd(seen, NULL, NULL, GrB_BOR_UINT64, seen, next, NULL)
         LAGr_apply(not_seen, NULL, NULL, GrB_BNOT_UINT64, seen, NULL)
 
-        print_bit_matrices(frontier, next, seen, popcount, sp);
+        // next_popcount *= level expressed as next_popcount *= level_v
+        LAGr_eWiseMult(next_popcount, NULL, NULL, GrB_TIMES_UINT64, next_popcount, level_v, NULL)
+        LAGr_eWiseAdd(sp, NULL, NULL, GrB_PLUS_UINT64, sp, next_popcount, NULL)
+
+        print_bit_matrices(frontier, next, seen, next_popcount, sp);
 
         // frontier = next
         LAGr_Matrix_dup(&frontier, next)
     }
+    // compsize = reduce(seen, row -> next_popcount(row))
+    GrB_Matrix_apply(Seen_PopCount, NULL, NULL, op_popcount, seen, NULL);
+    LAGr_reduce(compsize, NULL, NULL, GxB_PLUS_UINT64_MONOID, Seen_PopCount, NULL)
+    GxB_print(compsize, GxB_SHORT);
 
     LAGRAPH_FREE_ALL ;
     LAGraph_finalize ( ) ;
